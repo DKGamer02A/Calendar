9b0f3bfcb7b41f242b8c26029145752df9af75b6
DKGamer02AI <112185917+DKGamer02AI@users.noreply.github.com>
2022-09-04 11:15:56 +0530

    Create Calender.py

---
+++ Calender.py
@@ -0,0 +1,8 @@
+#Calander 
+
+import calendar as c
+from importlib import import_module
+year = int(input("Which year's Clander u want"))
+month= int(input("Which Months clander u want"))
+
+print(c.month(year, month))
